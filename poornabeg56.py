681cca0c40a7c78d91a704e4edf97749b38f773f
pedireddy <41773732+pedireddy@users.noreply.github.com>
2018-08-30 11:22:37 +0530

    Create poornabeg56.py

---
+++ poornabeg56.py
@@ -0,0 +1,7 @@
+x=input()
+z=list(x)
+if ("1" in z)or("2" in z)or("3" in z)or("4" in z)or("5" in z)or("6" in z)or("7" in z)or("8" in z)or("9" in z)or("0" in z):
+    print("Yes")
+else:
+    print("No")
+
